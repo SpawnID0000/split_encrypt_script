a69ffc600a26f76c97805ad830b5c201729d49fa
SpawnID0000 <142523337+SpawnID0000@users.noreply.github.com>
2023-12-18 13:41:27 -0800

    Update split_encrypt_script.py
    Adding multiprocessing capability for faster operation and also switched encryption from GnuPG to Python's built-in cryptography libraries

--- split_encrypt_script.py
+++ split_encrypt_script.py
@@ -5,27 +5,103 @@ import zipfile
 import shlex
 import logging
 import time
+from multiprocessing import Pool
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 from pathlib import Path
 
-# Initialize logging
-script_name = os.path.splitext(os.path.basename(__file__))[0]
-log_file = f"{script_name}_log.txt"
-logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# AES encryption function
+def aes_encrypt(input_data, passphrase):
+    backend = default_backend()
+    key = passphrase.ljust(32)[:32].encode()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=backend)
+    encryptor = cipher.encryptor()
 
-# Start time for duration calculation
-start_time = time.time()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(input_data) + padder.finalize()
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Split and encrypt audio files. \n\nUsage: python3 split_encrypt_script.py path/to/Music path/to/output',
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('input_dir', type=str, help='Path to the input directory')
-parser.add_argument('output_dir', type=str, help='Path to the output directory')
-args = parser.parse_args()
+    return iv + encryptor.update(padded_data) + encryptor.finalize()
 
-# Supported audio formats
-audio_formats = ['.mp3', '.m4a', '.flac', '.opus']
+# Function to process a single file
+def process_file(file_info):
+    input_dir, output_dir, file = file_info
+    file_path = os.path.join(input_dir, file)
+    file_name_no_ext, file_ext = os.path.splitext(file)
 
-# Function to copy directory structure
+    if file.startswith('.') or file.lower() == 'folder.jpg':
+        return f"Skipped file: {file}"
+
+    try:
+        file_name_part_ = os.path.join(output_dir, file_name_no_ext + "_part_")
+        split_command = f"split -b 475k {shlex.quote(file_path)} {shlex.quote(file_name_part_)}"
+        subprocess.run(split_command, shell=True)
+
+        for part_file in os.listdir(output_dir):
+            if part_file.startswith(file_name_no_ext + "_part_"):
+                full_path = os.path.join(output_dir, part_file)
+                with open(full_path, 'rb') as f:
+                    file_data = f.read()
+                encrypted_data = aes_encrypt(file_data, file_name_no_ext + file_ext)
+                encrypted_file_name = f"{part_file}{file_ext}.aes"
+                with open(os.path.join(output_dir, encrypted_file_name), 'wb') as f:
+                    f.write(encrypted_data)
+                os.remove(full_path)
+
+        output_zip_path = os.path.join(output_dir, file_name_no_ext + '.zip')
+        with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for aes_file in os.listdir(output_dir):
+                if aes_file.startswith(file_name_no_ext + "_part_") and aes_file.endswith('.aes'):
+                    zipf.write(os.path.join(output_dir, aes_file), aes_file)
+                    os.remove(os.path.join(output_dir, aes_file))
+
+        print(f"Finished processing: {file}")
+
+        return f"Processed file: {output_zip_path}"
+
+    except Exception as e:
+        return f"Error processing file {file}: {e}"
+
+def main():
+    script_name = os.path.splitext(os.path.basename(__file__))[0]
+    log_file = f"{script_name}_log.txt"
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Split and encrypt audio files. \n\nUsage: python3 split_encrypt_script.py path/to/Music path/to/output',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('input_dir', type=str, help='Path to the input directory')
+    parser.add_argument('output_dir', type=str, help='Path to the output directory')
+    args = parser.parse_args()
+
+    print("Copying directory structure...")
+    copy_directory_structure(args.input_dir, args.output_dir)
+
+    files_to_process = []
+    for root, dirs, files in os.walk(args.input_dir):
+        for file in files:
+            if file.startswith('.') or file.lower() == 'folder.jpg' or not any(file.endswith(ext) for ext in ['.mp3', '.m4a', '.flac', '.opus']):
+                continue
+            files_to_process.append((root, os.path.join(args.output_dir, os.path.relpath(root, args.input_dir)), file))
+
+    print("Processing files...")
+    with Pool() as pool:
+        results = pool.map(process_file, files_to_process)
+
+    for result in results:
+        if "Error" in result:
+            logging.error(result)
+        else:
+            # Uncomment the below line if detailed logging for each file is needed.
+            # logging.info(result)
+            print(result)
+
+    total_duration = time.time() - start_time
+    logging.info(f"Total script execution duration: {total_duration:.2f} seconds")
+    print("Processing complete!")
+
 def copy_directory_structure(src, dest):
     for root, dirs, files in os.walk(src):
         for directory in dirs:
@@ -34,70 +110,5 @@ def copy_directory_structure(src, dest):
             output_dir_path = os.path.join(dest, relative_path)
             os.makedirs(output_dir_path, exist_ok=True)
 
-# Copying directory structure
-print("Copying directory structure...")
-copy_directory_structure(args.input_dir, args.output_dir)
-
-# Process each file
-print("Processing files...")
-file_count = 0
-for root, dirs, files in os.walk(args.input_dir):
-    for file in files:
-        if file.startswith('.') or file.lower() == 'folder.jpg':  # Skip hidden files and 'Folder.jpg'
-            continue
-
-        if any(file.endswith(ext) for ext in audio_formats):
-            try:
-                print(f"Processing audio file: {file}")
-                file_path = os.path.join(root, file)
-                output_root = os.path.relpath(root, args.input_dir)
-                output_dir = os.path.join(args.output_dir, output_root)
-                file_name_no_ext, file_ext = os.path.splitext(file)
-
-                # Split file - Use shlex.quote() for safe shell execution
-                file_name_part_ = os.path.join(output_dir, file_name_no_ext + "_part_")
-                split_command = "split -b 475k {} {}".format(shlex.quote(file_path), shlex.quote(file_name_part_))
-                subprocess.run(split_command, shell=True)
-
-                # Encrypt file
-                passphrase = file_name_no_ext  # Filename as passphrase
-                gpg_path = "/usr/local/bin/gpg"  # Replace with your actual path to gpg
-
-                encrypted_files_to_delete = []
-
-                for part_file in os.listdir(output_dir):
-                    if part_file.startswith(file_name_no_ext + "_part_"):
-                        full_path = os.path.join(output_dir, part_file)
-                        result = subprocess.run([gpg_path, "-c", "--cipher-algo", "AES256", "--batch", "--yes", "--passphrase", passphrase, full_path])
-
-                        # If successful, store the encrypted file path
-                        if result.returncode == 0:
-                            encrypted_files_to_delete.append(full_path + '.gpg')
-
-                # Create ZIP package for each audio file
-                output_zip_path = os.path.join(output_dir, file_name_no_ext + '.zip')
-                with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-                    for encrypted_file in encrypted_files_to_delete:
-                        zipf.write(encrypted_file, os.path.basename(encrypted_file))
-                        os.remove(encrypted_file)  # Delete the encrypted file after adding to ZIP
-
-                # Delete the original unencrypted split parts
-                for part_file in os.listdir(output_dir):
-                    if part_file.startswith(file_name_no_ext + "_part_") and not part_file.endswith('.gpg'):
-                        os.remove(os.path.join(output_dir, part_file))
-
-                print(f"Created ZIP package: {output_zip_path}")
-                #logging.info(f"Processed file: {output_zip_path}")
-                file_count += 1
-
-            except Exception as e:
-                logging.error(f"Error processing file {file_path}: {e}")
-
-# Calculate total duration
-total_duration = time.time() - start_time
-
-# Log total files processed and total duration
-logging.info(f"Total files processed: {file_count}")
-logging.info(f"Total script execution duration: {total_duration:.2f} seconds")
-
-print("Processing complete!")
+if __name__ == "__main__":
+    main()
